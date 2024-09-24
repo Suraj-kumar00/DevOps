ea131267652264da50ddc876401175c03c7eb635
Suraj <suraj.ukumar.p@gmail.com>
2024-09-24 20:14:48 +0530

    feat: The directory on the docs of all topics are created.

--- docs/intro.md
+++ docs/intro.md
@@ -1,5 +1,12 @@
 ---
 sidebar_position: 1
+label: Introduction
+title: DevOps
+description: Understanding DevOps
 ---
 
 # DevOps
+
+> Hey there, My name is Suraj and I'm documenting my Cloud/DevOps learning journely here.
+
+## Understanding DevOps
--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -25,8 +25,7 @@ const config = {
           sidebarPath: "./sidebars.ts",
           // Please change this to your repo.
           // Remove this to remove the "edit this page" links.
-          editUrl:
-            "https://github.com/facebook/docusaurus/tree/main/packages/create-docusaurus/templates/shared/",
+          editUrl: "https://github.com/Suraj-kumar00/DevOps/issues",
         },
         blog: false,
         // blog: {
